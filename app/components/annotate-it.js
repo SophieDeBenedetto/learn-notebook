18f4331c7dfce0e37e46843b97893e4ef20947c1
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-12-12 17:52:01 -0500

    can create and retrieve notes from api, can't update or delete them

--- app/components/annotate-it.js
+++ app/components/annotate-it.js
@@ -3,7 +3,27 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   didInsertElement: function() {
-    alert("HI");
-    $('body').annotator()
+    let id = this.id;
+    $('#readme').annotator().annotator('addPlugin', 'Store', {
+      prefix: 'http://localhost:3000/api/v1',
+      //Attach the uri of the current page to all annotations to allow search.
+      annotationData: {
+        // set uri here to scope annotation
+        // 'uri': 'http://this/document/only',
+        'readme_id': id
+      },
+
+      urls: {
+        search: '/annotations/search'
+      },
+
+      loadFromSearch: {
+        // 'limit': 20,
+        // 'all_fields': 1,
+        // 'readme_id': this.id
+        'uri': 'http://localhost:4200/readmes/' + this.id
+      }
+
+    })
   }
 });
